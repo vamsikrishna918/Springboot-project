ee2977056b7c9946e219c67a0fb9f4936a94708a
VamsiKrishna Annadanam <67264274+vamsikrishna918@users.noreply.github.com>
2023-11-30 01:25:57 +0530

    Update controllerclass.java

--- src/main/java/com/demo/project/springboot/demo/project/controllerclass.java
+++ src/main/java/com/demo/project/springboot/demo/project/controllerclass.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/test")
 public class controllerclass {
 
 	
-		@GetMapping("/test")
+		@GetMapping
 		public String hello(){
 		return "welcome to springboot application";
 		}
